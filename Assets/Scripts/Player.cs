2548ec336eed86d8ab22638d75fc96aabf6ba727
Jogio <jonavaz18@gmail.com>
2020-12-07 15:44:36 -0600

    Creación del escenario con 2D extras

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 mov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hola mundo!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mov = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+           );
+
+        gameObject.transform.position = mov * Time.deltaTime;
+
+    }
+}
